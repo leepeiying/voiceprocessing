57b65064b5df7fcc6158571a62cccae65e015c41
school msi <penny868181@hotmail.com.tw>
2022-01-04 22:41:13 +0800

    update2 voice2

--- voice2.py
+++ voice2.py
@@ -1,10 +1,13 @@
 import os
 from os.path import isfile, join
 import subprocess
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+import numpy as np
 
-
-#大量改檔名
 """
+#大量改檔名
+
 path='儲存的路徑' #這就是欲進行檔名更改的檔案路徑，路徑的斜線是為/，要留意下！
 files=os.listdir(path)
 print('files') #印出讀取到的檔名稱，用來確認自己是不是真的有讀到
@@ -16,9 +19,10 @@ for i in files: #因為資料夾裡面的檔案都要重新更換名稱
 	os.rename(oldname,newname)
 	print(oldname+'>>>'+newname) #印出原名與更名後的新名，可以進一步的確認每個檔案的新舊對應
 	n=n+1 #當有不止一個檔案的時候，依次對每一個檔案進行上面的流程，直到更換完畢就會結束
-"""
+
 
 #因為檔名一樣就自動讀取,之後檔名不一樣可以統一改檔名
+#多個轉檔
 count = 0
 path = 'C:\\Users\\user\\voiceprocessing\\internship\\VOA_audio\\mp3'
 dirlist = os.listdir(path)
@@ -31,6 +35,45 @@ for i in dirlist:
         # for mp3 files --> .wav files
         input = f'ffmpeg -i C:\\Users\\User\\voiceprocessing\\internship\\VOA_audio\\mp3\\{count}_mp3.mp3 -ar 16000 -ac 1 -acodec pcm_s16le C:\\Users\\User\\voiceprocessing\\internship\\VOA_audio\\wav\\{count}_wav.wav'
         subprocess.call(input, shell = True)
+"""
 
+#讀取單個指定wav檔
+input_file = "internship\\VOA_audio\\wav\\2_wav.wav" 
+rate, data = read(input_file)
+#查看取樣頻率
+print("Sample rate: {} Hz".format(rate))
+# 查看取樣資料的類型
+print("Data type: {}".format(data.dtype))
+# 繪製前 1024 點資料的波形圖
+plt.figure(figsize=(15, 5))
+plt.plot(data[0:1024])
+plt.show()
+#經過傅立葉轉換之後，繪製頻譜圖（spectrum）
+# 傅立葉轉換
+from scipy.fftpack import fft
+dataFFT = fft(data[0:1024])
+dataFFTAbs = abs(dataFFT[1:512])
 
+# 繪製頻譜圖
+plt.figure(figsize=(15, 5))
+plt.plot(dataFFTAbs, 'r')
+plt.show()
+#將全部資料的波形圖與時頻譜圖（spectrogram）繪製在一起
+# 產生時間資料
+time = np.arange(0, len(data)) / rate
 
+plt.figure(figsize=(15, 5))
+
+# 繪製波形圖
+plotA = plt.subplot(211)
+plotA.plot(time, data)
+plotA.set_ylabel("Amplitude")
+plotA.set_xlim(0, len(data) / rate)
+
+# 繪製時頻譜圖
+plotB = plt.subplot(212)
+plotB.specgram(data, NFFT=1024, Fs=rate, noverlap=900)
+plotB.set_ylabel("Frequency")
+plotB.set_xlabel("Time")
+
+plt.show()
